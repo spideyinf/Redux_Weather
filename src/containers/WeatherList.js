48661b4632f41ee4562f4aee73ed4b8d5afd1e6a
Hoang Phuc <phuchoang.inf@gmail.com>
2018-05-15 11:10:22 +0700

    redux-promise cont. react-sparklines

---
+++ src/containers/WeatherList.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Chart from '../components/Chart';
+
+class WeatherList extends Component {
+  renderWeather(data) {
+    const name = data.city.name;
+    const temps = _.map(
+      data.list.map(weather => weather.main.temp),
+      temp => temp - 273
+    );
+    const pressures = data.list.map(weather => weather.main.pressure);
+    const humidities = data.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" units="°C" />
+        </td>
+        <td>
+          <Chart data={pressures} color="red" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="lightblue" units="%" />
+        </td>
+      </tr>
+    );
+  }
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temparature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+  // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
+    // = return state.concat[action.payload.data];
+    default:
+      return state;
+  }
 };
